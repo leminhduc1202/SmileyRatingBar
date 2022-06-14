0bd1f0c860af6be2efdf1aabe9895a46d83c0a88
duc.le@madison-technology.com <duc.le@madison-technology.com>
2022-06-14 17:38:22 +0700

    add chip tag signup on boarding

--- app/src/main/java/com/mdapp/smileyrating/ipcupdate/signup/SignUpFragmentChoseInterest.kt
+++ app/src/main/java/com/mdapp/smileyrating/ipcupdate/signup/SignUpFragmentChoseInterest.kt
@@ -1,10 +1,21 @@
 package com.mdapp.smileyrating.ipcupdate.signup
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
+import android.graphics.Typeface
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getColor
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import com.mdapp.smileyrating.R
 import com.mdapp.smileyrating.databinding.FragmentWelcome1Binding
 import com.mdapp.smileyrating.databinding.FrgamentChoseInterestBinding
 
@@ -24,5 +35,23 @@ class SignUpFragmentChoseInterest: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
     }
+
+
+    private fun initView() {
+       val textArray : Array<String> = resources.getStringArray(R.array.text_tag)
+        for (textTag in textArray ){
+            val chip = Chip(context)
+            chip.text = textTag
+            chip.typeface = Typeface.create(ResourcesCompat.getFont(requireContext(), R.font.ikanosans_light), Typeface.NORMAL)
+            chip.textSize = 20f
+            chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            chip.chipBackgroundColor = ColorStateList.valueOf(getColor(requireContext(),R.color.text_color_welcome))
+            binding.ChipGroup.addView(chip)
+        }
+    }
+
+
+
 }
