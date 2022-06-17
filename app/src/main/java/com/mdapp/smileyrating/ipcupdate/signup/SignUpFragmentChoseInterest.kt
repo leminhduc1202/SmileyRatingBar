5e70b46f6e4653bfc3b5c6aaad554ddb26bdb1a2
duc.le@madison-technology.com <duc.le@madison-technology.com>
2022-06-17 09:50:20 +0700

    add style chip choice

--- app/src/main/java/com/mdapp/smileyrating/ipcupdate/signup/SignUpFragmentChoseInterest.kt
+++ app/src/main/java/com/mdapp/smileyrating/ipcupdate/signup/SignUpFragmentChoseInterest.kt
@@ -5,16 +5,14 @@ import android.graphics.Typeface
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat.getColor
 import androidx.core.content.res.ResourcesCompat
-import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipDrawable
+import com.google.android.material.chip.ChipGroup
 import com.mdapp.smileyrating.R
 import com.mdapp.smileyrating.databinding.FrgamentChoseInterestBinding
 
@@ -41,7 +39,8 @@ class SignUpFragmentChoseInterest: Fragment() {
     private fun initView() {
        val textArray : Array<String> = resources.getStringArray(R.array.text_tag)
         for (textTag in textArray ) {
-            val chip = Chip(context)
+
+            val chip = layoutInflater.inflate(R.layout.layout_chip_choice, binding.chipGroup, false) as Chip
             chip.text = textTag
             chip.typeface = Typeface.create(
                 ResourcesCompat.getFont(requireContext(), R.font.ikanosans_light),
@@ -53,25 +52,10 @@ class SignUpFragmentChoseInterest: Fragment() {
             chip.chipStartPadding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 18f,resources.displayMetrics)
             chip.chipEndPadding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 18f,resources.displayMetrics)
 
-//            chip.backgroundDrawable = ChipDrawable.createFromAttributes(requireContext(),null, 0, com.google.android.material.R.style.Widget_MaterialComponents_Chip_Choice)
-//            chip.chipBackgroundColor =
-//                ColorStateList.valueOf(getColor(requireContext(), R.color.white))
+
 
             binding.chipGroup.addView(chip)
 
-//            binding.chipGroup.checkedChipIds.forEach { _ ->
-//                val userText: String = chip.text.toString()
-//                Toast.makeText(requireContext(), userText, Toast.LENGTH_LONG).show()
-//                chip.chipBackgroundColor =
-//                    ColorStateList.valueOf(getColor(requireContext(), R.color.text_color_welcome))
-//            }
-
-            chip.setOnClickListener {
-                val userText: String = chip.text.toString()
-                Toast.makeText(requireContext(), userText, Toast.LENGTH_LONG).show()
-                chip.chipBackgroundColor =
-                    ColorStateList.valueOf(getColor(requireContext(), R.color.text_color_welcome))
-            }
         }
     }
 
